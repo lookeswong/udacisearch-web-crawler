14931da659081091f7be77f8418f792db6db0ffb
lookeswong <lookeswong@gmail.com>
2021-09-15 14:26:48 +0100

    upload all files

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -0,0 +1,54 @@
+package com.udacity.webcrawler.profiler;
+
+import  java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Objects;
+
+/**
+ * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
+ * annotation. If they are, the method interceptor records how long the method invocation took.
+ */
+final class ProfilingMethodInterceptor implements InvocationHandler {
+
+  private final Clock clock;
+  private final Object targetObject;
+  private final ProfilingState profilingState;
+
+  // TODO: You will need to add more instance fields and constructor arguments to this class.
+  ProfilingMethodInterceptor(Clock clock, Object targetObject, ProfilingState profilingState) {
+    this.clock = Objects.requireNonNull(clock);
+    this.targetObject = Objects.requireNonNull(targetObject);
+    this.profilingState = Objects.requireNonNull(profilingState);
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+    // TODO: This method interceptor should inspect the called method to see if it is a profiled
+    //       method. For profiled methods, the interceptor should record the start time, then
+    //       invoke the method using the object that is being profiled. Finally, for profiled
+    //       methods, the interceptor should record how long the method call took, using the
+    //       ProfilingState methods.
+    Object result;
+    Instant start = null;
+    boolean isProfiled = method.getAnnotation(Profiled.class) != null;
+    if (isProfiled) {
+      start = clock.instant();
+    }
+    try {
+      result = method.invoke(targetObject, args);
+    } catch (InvocationTargetException ex) {
+      throw ex.getTargetException();
+    } finally {
+      if (isProfiled) {
+        Duration duration = Duration.between(start, clock.instant());
+        profilingState.record(targetObject.getClass(), method, duration);
+      }
+    }
+
+    return result;
+  }
+}
